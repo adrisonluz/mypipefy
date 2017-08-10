52113107899cdcbd1e1a858bc85dea113dacf362
Adrison Luz <adrison701@gmail.com>
2017-08-10 14:20:27 -0300

    Tela de entrada criada, favicon e logo criado.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="msg_home">
+                        <imge>
+                            <img src="{{ asset('img/mypipefy.png') }}">
+                        </image>
+
+                        <div class="welcome">
+                            <h1>MyPipefy</h1>
+                            <p>Seja bem vindo!</p>
+                        </div>
+                    </div> 
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('img/mypipefy_favicon.png') }}"/>
 
     <!-- Styles -->
     @stack('styles')
