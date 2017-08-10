a9cebe5fed7707098426c537083dd2a1bf55ad04
Jonatan Colussi <jcolussi@reweb.com.br>
2017-08-10 17:51:50 -0300

    Load das tabelas [parcial]

--- public/js/functions.js
+++ public/js/functions.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
 	    	order: [[4, 'asc']],
 	    	processing: true,
 	        serverSide: true,
-	        ajax: route
+	        ajax: route,
+	        oLanguage: {
+		        sProcessing: '<img src="http://localhost/mypipefy/public/img/mypipefy.png" title="Loading ..." class="animated infinite flip"/>',
+		    }
 	    });
     });
 } );
