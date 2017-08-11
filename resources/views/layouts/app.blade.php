8594604eac48ce4f6a507d936685b214540fe575
Nicolas Renard <nicolasrenard1999@gmail.com>
2017-08-11 16:55:44 -0300

    Ajustes menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,33 +20,65 @@
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
-                <div class="navbar-header">
 
+                <div class="navbar-header mobile-header">
+                  <img class="img-responsive logo" src="{{ asset('img/mypipefy.png') }}" alt="MyPipefy">
+                  <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                      <span class="text-primary">{{ config('app.name', 'Laravel') }}</span>
+                  </a>
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                      <span class="sr-only">Toggle Navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  @if (isset($me))
+                    <img src="{{$me->avatar_url}}" title="{{$me->name}}" class="avatar img-responsive img-thumbnail">
+                  @endif
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/dashboard') }}">
-                        <span class="text-primary">{{ config('app.name', 'Laravel') }}</span>
-                        @if (isset($me))
-                            | {{$me->name}} <img src="{{$me->avatar_url}}" title="{{$me->name}}" class="avatar img-responsive img-thumbnail pull-left">
-                        @endif
-                    </a>
                 </div>
-
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                  <ul class="nav navbar-nav">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <li><a href="{{ route('dashboard.team') }}">Team</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                  </ul>
+
+                </div>
+                <div class="collapse navbar-collapse">
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                      <!-- Branding Image -->
+                      <img class="img-responsive logo" src="{{ asset('img/mypipefy.png') }}" alt="MyPipefy">
+                      <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                          <span class="text-primary">{{ config('app.name', 'Laravel') }}</span>
+                      </a>
+                      <!-- Left Side Of Navbar -->
+                      <ul class="nav navbar-nav">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
@@ -74,7 +106,17 @@
                                 </ul>
                             </li>
                         @endif
-                    </ul>
+                      </ul>
+
+                    </div>
+                    <!-- Right Side Of Navbar -->
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="user-name">
+                          @if (isset($me))
+                              | {{$me->name}} <img src="{{$me->avatar_url}}" title="{{$me->name}}" class="avatar img-responsive img-thumbnail pull-left">
+                          @endif
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
