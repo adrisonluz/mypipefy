17c166053dce287b6fb1abddf279899cdc3e13df
Adrison Luz <adrison701@gmail.com>
2017-08-11 09:12:26 -0300

    Ajuste layout de login e register, inserido logo.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
                 <div class="panel-body">
+                    <div class="msg_home">
+                        <imge>
+                            <img src="{{ asset('img/mypipefy.png') }}">
+                        </image>
+
+                        <div class="welcome">
+                            <h1>MyPipefy</h1>
+                            <p>Seja bem vindo!</p>
+                        </div>
+                    </div>
+
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
