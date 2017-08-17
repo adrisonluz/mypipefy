ff95c5013b92012932aa87fbce1c477e1f61a9d7
Nicolas Renard <nicolasrenard1999@gmail.com>
2017-08-17 18:51:48 -0300

    Modificações pagina de Config.

---
+++ public/js/config.js
@@ -0,0 +1,53 @@
+$(function(){
+  $(".container").delegate('.pipefy-users .add-team', 'click', function(event) {
+      var pipefy_id = $(this).data('pipefyid');
+      var $row = $(this).parent().parent();
+      var $clone = $row.clone();
+      $clone.find('button').removeClass('add-team').addClass('pending');
+      $row.remove();
+      $(".my-team").prepend($clone);
+      $('p.not-have').fadeOut();
+  });
+
+   $(".container").delegate('.my-team button', 'click', function(event) {
+      var pipefy_id = $(this).data('pipefyid');
+      var $row = $(this).parent().parent();
+      var $clone = $row.clone();
+      $clone.find('button').addClass('add-team').removeClass('pending');
+      $row.remove();
+      $(".pipefy-users").prepend($clone);
+
+      var numberRowTeam = $('.my-team .row').length;
+      if(!numberRowTeam){$('p.not-have').fadeIn();}
+  });
+
+  $("[name=filter]").on('keyup', function(){
+      var pesquisa = $(this).val();
+      pesquisa = pesquisa.toUpperCase();
+      $(".pipefy-users .name").each(function(){
+          if($(this).text().toUpperCase().indexOf(pesquisa) >= 0)
+              $(this).parent().show('fast');
+          else
+              $(this).parent().hide('fast');
+      });
+  });
+
+  $("[name=memberFilter]").on('keyup', function(){
+      var pesquisa = $(this).val();
+      pesquisa = pesquisa.toUpperCase();
+      $(".my-team .name").each(function(){
+          if($(this).text().toUpperCase().indexOf(pesquisa) >= 0)
+              $(this).parent().show('fast');
+          else
+              $(this).parent().hide('fast');
+      });
+  });
+  
+  $('.config img.avatar.img-responsive.img-thumbnail').each(function(){
+  	var alturaImage = $(this).height();
+  	var tamanhoImage = $(this).width();
+  	if(alturaImage < 60 || tamanhoImage > 75){
+  		$(this).attr('src','http://localhost/mypipefy/public/img/mypipefy.png');
+  	}
+  });
+});
--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -1,57 +1,9 @@
 @extends('layouts.app')
 @section('content')
 @push('scripts')
-    <script>
-        $(".container").delegate('.pipefy-users .add-team', 'click', function(event) {
-            var pipefy_id = $(this).data('pipefyid');
-            var $row = $(this).parent().parent();
-            var $clone = $row.clone();
-            $clone.find('button').removeClass('add-team').addClass('pending');
-            $row.remove();
-            $(".my-team").prepend($clone);
-        });
-
-         $(".container").delegate('.my-team button', 'click', function(event) {
-            var pipefy_id = $(this).data('pipefyid');
-            var $row = $(this).parent().parent();
-            var $clone = $row.clone();
-            $clone.find('button').addClass('add-team').removeClass('pending');
-            $row.remove();
-            $(".pipefy-users").prepend($clone);
-        });
-
-        $("[name=filter]").on('keyup', function(){
-            var pesquisa = $(this).val();
-            pesquisa = pesquisa.toUpperCase();
-            $(".pipefy-users .name").each(function(){
-                if($(this).text().toUpperCase().indexOf(pesquisa) >= 0)
-                    $(this).parent().show('fast');
-                else
-                    $(this).parent().hide('fast');
-            });
-            ajustaLayout();
-        });
-    </script>
+<script src="{{ asset('js/config.js') }}"></script>
 @endpush
-@push('styles')
-    <style>
-        .my-team button:before{
-            content: "\f00d";
-            margin-right: 2px;
-            font-family: FontAwesome;
-        }
-        .my-team button.on-team:after{
-            content: 'Remover do time'
-        }
-        .my-team button.pending:after{
-            content: 'Aguardando aprovação'
-        }
-        .pipefy-users button.add-team:after{
-            content: 'Vem para o meu Time!';
-        }
-    </style>
-@endpush
-<div class="container">
+<div class="container config">
     <div class="row">
         {{-- My Team --}}
         <div class="col-md-6">
@@ -59,23 +11,18 @@
                 <div class="panel-heading">
                     <h1>Meu time</h1>
                 </div>
-                <div class="panel-body">
-                    <div class="my-team">
-                        {{-- @for($i=0;$i<4;$i++) --}}
-                        {{-- <div class="row" style="border-bottom: 1px solid #ccc">
-                            <div class="col-md-1">
-                                <img src="{{$me->avatar_url}}" title="{{$me->name}}" class="avatar img-responsive img-thumbnail">
-                            </div>
-                            <div class="col-md-3">
-                                <span>{{ $me->name }}</span>
-                                <span>{{ ($me->username != '') ? '@'.$me->username : '' }}</span>
-                                <span>{{ $me->email }}</span>
-                            </div>
-                            <div class="col-md-3">
-                                <button class="btn btn-default on-team" data-pipefyid="{{ $me->id }}"></button>
+                <div class="panel-body myteam">
+                    <div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Pesquisar Membros" name="memberFilter">
+                                </div>
                             </div>
                         </div>
-                        @endfor --}}
+                      </div>
+                    <div class="my-team">
+                      <p class="not-have">Não tem ninguém no seu time. Convide Alguém.</p>
                     </div>
                 </div>
             </div>
@@ -86,9 +33,9 @@
                 <div class="panel-heading">
                     <h1>Usuários</h1>
                 </div>
-                <div class="panel-body">
+                <div class="panel-body myteam">
                     <div>
-                        <div class="row" style="border-bottom: 1px solid #ccc">
+                        <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <input type="text" class="form-control" placeholder="Pesquisar usuários" name="filter">
@@ -98,16 +45,22 @@
                     </div>
                     <div class="pipefy-users">
                         @foreach($users as $user)
-                        <div class="row" style="border-bottom: 1px solid #ccc">
-                            <div class="col-md-1">
+                        <div class="row">
+                            <div class="col-md-2">
                                 <img src="{{$user->avatar_url}}" title="{{$user->name}}" class="avatar img-responsive img-thumbnail">
                             </div>
-                            <div class="col-md-3 name">
-                                <span>{{ $user->name }}</span>
-                                <span>{{ ($user->username != '') ? '@'.$user->username : '' }}</span>
+                            <div class="col-md-5 name">
+                              @if($user->name)
+                                <span class="name-user">{{ $user->name }}</span>
+                              @endif
+                              @if($user->username)
+                                <span>{{ '@'.$user->username }}</span>
+                              @endif
+                              @if($user->email)
                                 <span>{{ $user->email }}</span>
+                              @endif
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-5">
                                 <button class="btn btn-default add-team" data-pipefyid="{{ $user->pipefy_id }}"></button>
                             </div>
                         </div>
