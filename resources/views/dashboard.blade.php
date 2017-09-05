0633cf7b4d9af4ca79445923fcb6dea4255759cf
Jonatan Colussi <jcolussi@reweb.com.br>
2017-09-05 16:38:11 -0300

    Separação da partial de usuários

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,55 +30,7 @@
       </div>
 
       <div class="panel-body">
-        <div class="col-md-8 div-table">
-          <div class="loader-tables" style="display:none;">
-            <div class="load-pages">
-              <div class="gif-loader">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </div>
-          </div>
-          <table class="table display responsive no-wrap table-striped table-bordered tableDashboard" data-route="{{ route('api.get_cards_user_id', ['userId' => $me->id]) }}">
-            <thead class="thead-inverse">
-              <tr>
-                <td>ID</td>
-                <td>Pipe</td>
-                <td>Título</td>
-                <td>Cliente</td>
-                <td>DUE</td>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
-          <button type="button" name="button" class="buttonUpdateTable btn btn-primary"><i class="fa fa-refresh"></i> Atualizar cards</button>
-        </div>
-
-        <div class="col-md-4 div-calendar">
-          <div class="panel panel-info">
-            <div class="panel-heading">
-              <h4><strong><i class="fa fa-calendar"></i> Tarefas Agendadas</strong></h4>
-            </div>
-            <div class="panel-body">
-            <div class='calendar calendar_{{$me->id}}' data-userid="{{$me->id}}" data-route="{{route('api.get_cards_user')}}">
-              </div>
-              <div class="load-calendario" style="display:none;">
-                <div class="load-pages">
-                  <div class="gif-loader">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        @include('partials.pipefyUser', $pipefyUser)
       </div>
     </div>
   </div>
