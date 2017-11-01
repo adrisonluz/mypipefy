b18ed0f73a7580e8d6f6dfdb2c6188128a5d8a34
Jonatan Colussi <jcolussi@reweb.com.br>
2017-11-01 16:28:37 -0200

    Login com o Google

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
@@ -227,6 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
