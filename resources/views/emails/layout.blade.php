68d048f292bafc118f531c81c32de0024a7a9bc4
Nicolas Renard <nrenard@reweb.com.br>
2017-11-20 16:26:05 -0200

    Criação de email.

---
+++ resources/views/emails/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div>
+
+	        <div style="border:1px solid #cccccc;margin:10px">
+	<br>
+	            <div style="margin-bottom:20px;text-align:center;padding:10px;">
+	                    <img src="https://ci3.googleusercontent.com/proxy/PiC__wMMvFBcpfgCtk4YTq7L9MTCcu0o4rBw0H-gqA6EGyZNw2RsdjEouyCQgqqL2AbyCHnu1yEq2g-Ka8cHjq6M3kVHtEUwSCt4E8Iu8v4=s0-d-e1-ft#http://mypipefy.reweb.com.br/img/mypipefy_logo_extenso.png" alt="" height="80">
+	            <hr style="width: 59%;border: 1px solid #cccccc;"></div>
+              @yield('content');
+	  					<div style="margin-top:20px;text-align:center;background:rgb(41,130,181);padding:10px">
+	                    <p style="color: #ffffff;font-size: 14px;">MyPipefy, todos os direitos reservados. Acesse: <a href="http://mypipefy.reweb.com.br" target="_blank" style="color: #ffffff;text-decoration: none;font-weight: bold;">http://mypipefy.reweb.com.br</a></p>
+
+	            </div>
+	        </div>
+
+	    </div>
+
+
+</body>
+</html>
